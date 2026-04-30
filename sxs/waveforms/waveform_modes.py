d44f1b5827fb9168221e557fc0b64851841433ee
Aniket Khairnar <akhairna@go.olemiss.edu>
2026-04-30 16:18:49 -0500

    Add optional processing

--- sxs/waveforms/waveform_modes.py
+++ sxs/waveforms/waveform_modes.py
@@ -1446,6 +1446,83 @@ class WaveformModes(WaveformMixin, TimeSeries):
         result = result.line_subtraction(treat_as_zero=treat_as_zero)
         return result
 
+    def preprocess_no_mem(self, t1=0, t2=500, t3=None, t4=None, dt=None, taper=False, pad=False, transition=False, **kwargs):
+        """Preprocess the data to prepare for FFT
+
+        This function is a convenience function that applies a series
+        of common preprocessing steps to the data before Fourier
+        transforming it.  The steps are:
+
+            1. `interpolate`
+            2. `taper`
+            3. `transition_to_constant`
+            4. `pad`
+
+        Parameters
+        ----------
+        t1 : float, optional
+            Time before which the data will be zeroed, after which the
+            taper will begin.  Default is 0 (not the first time step,
+            but t=0).
+        t2 : float, optional
+            Time at which the tapering will end.  Default is 500.
+        t3 : float, optional
+            Time at which the transition to a constant value will
+            begin.  Default is max_norm_time+100.
+        t4 : float, optional
+            Time after which the data will be constant.  Default is
+            max_norm_time+200.
+        dt : float, optional
+            Time step for the new time array.  Default is the smallest
+            time step in the input data.
+
+        Keyword Parameters
+        ------------------
+        evaluate_directions : array_like, optional
+            Directions at which to evaluate the waveform.  Default is
+            `None`, in which case the waveform modes are
+            pre-processed.  See `evaluate` for more information on the
+            meaning of other possible input values.
+        pad_length : tuple, optional
+            Length of padding to apply to the data.  Default option
+            will triple the length of the data.  (See `pad` for more
+            information.)
+        pad_mode : str, optional
+            Mode of padding.  Default value is `"edge"`.  (See `pad`
+            for more information.)
+        treat_as_zero : str, optional
+            How to treat the data at the boundaries.  Default value is
+            `"begin"`.  (See `line_subtraction` for more information.)
+        
+        All other keyword arguments are passed to `numpy.pad`.
+
+        Returns
+        -------
+        TimeSeries
+            New object with preprocessed data.
+
+        """
+        # Process arguments
+        t3 = t3 or self.max_norm_time() + 100
+        t4 = t4 or self.max_norm_time() + 200
+        dt = dt or np.min(np.diff(self.time))
+        evaluate_directions = kwargs.pop("evaluate_directions", None)
+        pad_length = kwargs.pop("pad_length", None)
+        pad_mode = kwargs.pop("pad_mode", "edge")
+
+        result = self.interpolate(np.arange(self.time[0], self.time[-1], dt))
+        if evaluate_directions is not None:
+            result = result.evaluate(evaluate_directions)
+        
+        if taper:
+            result = result.taper(t1, t2)
+        if transition:
+            result.transition_to_constant(t3, t4)
+        if pad:
+            result = result.pad(pad_length, mode=pad_mode, **kwargs)
+
+        return result
+
     def fourier_transform(self, theta_phi=None, total_mass=None, luminosity_distance=None):
         """Fourier transform the modes of a waveform.
 
